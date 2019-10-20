b83871fc7cc98896f64cd8a720f55bac3d294faf
zpc520 <1169078423@qq.com>
2019-10-20 22:50:13 +0800

    roles完成

--- src/components/roles.vue
+++ src/components/roles.vue
@@ -12,6 +12,7 @@
     <el-table :data="tableData" style="width: 100%">
       <el-table-column type="expand">
         <template v-slot:default="{row}">
+          <p v-if="row.children.length ===0">暂无数据</p>
           <el-row class="l1" v-for="l1 in row.children" :key="l1.id">
             <el-col :span="4">
               <el-tag @close="delRoles(row,l1.id)" closable>{{l1.authName}}</el-tag>
@@ -47,14 +48,26 @@
       <el-table-column prop="roleDesc" label="描述"></el-table-column>
       <el-table-column label="操作">
         <template v-slot:default="{row}">
-          <el-button @click="showEditDia(row)" size="small" type="primary" plain icon="el-icon-edit"></el-button>
-          <el-button size="small" type="danger" plain icon="el-icon-delete"></el-button>
-          <el-button size="small" type="success" plain icon="el-icon-check">分配权限</el-button>
+          <el-button
+            @click="showEditDia(row)"
+            size="small"
+            type="primary"
+            plain
+            icon="el-icon-edit"
+          ></el-button>
+          <el-button
+            @click="delRolesList(row.id)"
+            size="small"
+            type="danger"
+            plain
+            icon="el-icon-delete"
+          ></el-button>
+          <el-button @click="showShareRoles(row)" size="small" type="success" plain icon="el-icon-check">分配权限</el-button>
         </template>
       </el-table-column>
     </el-table>
     <!-- 添加模态框 -->
-    <el-dialog @close='closeDia' title="添加角色" :visible.sync="dialogVisible" width="40%">
+    <el-dialog @close="closeDia" title="添加角色" :visible.sync="dialogVisible" width="40%">
       <el-form ref="form" :model="form" label-width="80px" :rules="rules" status-icon>
         <el-form-item label="角色名称" prop="roleName">
           <el-input placeholder="请输入角色名称" v-model="form.roleName"></el-input>
@@ -69,7 +82,7 @@
       </span>
     </el-dialog>
     <!-- 编辑模态框 -->
-    <el-dialog @close='closeEdit' title="添加角色" :visible.sync="eidtVisible" width="40%">
+    <el-dialog @close="closeEdit" title="修改角色" :visible.sync="eidtVisible" width="40%">
       <el-form ref="editform" :model="editform" label-width="80px" :rules="rules" status-icon>
         <el-form-item label="角色名称" prop="roleName">
           <el-input placeholder="请输入角色名称" v-model="editform.roleName">{{this.editform.username}}</el-input>
@@ -83,6 +96,15 @@
         <el-button type="primary" @click="editRoles">确 定</el-button>
       </span>
     </el-dialog>
+    <!-- 分配权限模态框 -->
+    <el-dialog title="分配权限" :visible.sync="shareVisible" width="40%">
+      <!-- 树型控件 -->
+      <el-tree :data="data" show-checkbox default-expand-all node-key="id" ref="tree" :props="defaultProps"></el-tree>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button @click="shareRights" type="primary">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -92,22 +114,38 @@ export default {
     return {
       rules: {
         roleName: [
-          { required: true, message: '角色名称不能为空', trigger: ['blur', 'change'] }
+          {
+            required: true,
+            message: '角色名称不能为空',
+            trigger: ['blur', 'change']
+          }
         ],
         roleDesc: [
-          { required: true, message: '角色描述不能为空', trigger: ['blur', 'change'] }
+          {
+            required: true,
+            message: '角色描述不能为空',
+            trigger: ['blur', 'change']
+          }
         ]
       },
+      defaultProps: {
+        children: 'children',
+        label: 'authName'
+      },
+      data: [],
       tableData: [],
       dialogVisible: false,
       eidtVisible: false,
+      shareVisible: false,
+      rolesId: '',
       form: {
         roleName: '',
         roleDesc: ''
       },
       editform: {
         roleName: '',
-        roleDesc: ''
+        roleDesc: '',
+        id: ''
       }
     }
   },
@@ -151,14 +189,70 @@ export default {
         this.$message.error(meta.msg)
       }
     },
-    editRoles () {
-
+    async editRoles () {
+      const { meta } = await this.$axios.put(`roles/${this.editform.id}`, this.editform)
+      if (meta.status === 200) {
+        this.$message.success(meta.msg)
+        this.eidtVisible = false
+        this.getUserList()
+      } else {
+        this.$message.error(meta.msg)
+      }
     },
     closeDia () {
       this.$refs.form.resetFields()
     },
     closeEdit () {
       this.$refs.editform.resetFields()
+    },
+    async delRolesList (id) {
+      try {
+        await this.$confirm('亲，您确定要删除条信息吗？', '温馨提示', {
+          type: 'warning'
+        })
+        const { meta } = await this.$axios.delete(`roles/${id}`)
+        if (meta.status === 200) {
+          this.$message.success(meta.msg)
+          this.getUserList()
+        } else {
+          this.$message.error(meta.msg)
+        }
+      } catch (e) {
+
+      }
+    },
+    async showShareRoles (row) {
+      this.shareVisible = true
+      this.roleId = row.id
+      const { meta, data } = await this.$axios.get('rights/tree')
+      if (meta.status === 200) {
+        this.data = data
+        const ids = []
+        row.children.forEach(l1 => {
+          l1.children.forEach(l2 => {
+            l2.children.forEach(l3 => {
+              ids.push(l3.id)
+            })
+          })
+        })
+        this.$refs.tree.setCheckedKeys(ids)
+      } else {
+        console.log(meta.msg)
+      }
+    },
+    async shareRights () {
+      const ids = this.$refs.tree.getCheckedKeys()
+      const haflid = this.$refs.tree.getHalfCheckedKeys()
+      const rids = [...ids, ...haflid].join(',')
+      // 发送ajax请求
+      const { meta } = await this.$axios.post(`roles/${this.roleId}/rights`, { rids })
+      if (meta.status === 200) {
+        this.$message.success(meta.msg)
+        this.shareVisible = false
+        this.getUserList()
+      } else {
+        this.$message.error(meta.msg)
+      }
     }
   },
   created () {
